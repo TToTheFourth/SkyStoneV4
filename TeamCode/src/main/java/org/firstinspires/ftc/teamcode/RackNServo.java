353aa3125714e32a5f77f078e1c9f181698bc889
Clarekpd <Clarekpd@outlook.com>
2019-10-11 15:36:26 -0500

    added rack controls for Caden

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RackNServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RackNServo.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@TeleOp
 public class RackNServo extends LinearOpMode {
     private DcMotor rackMotor;
     private Servo squeezeServo;
