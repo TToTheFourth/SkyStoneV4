a742ff878aa9864b78a0eae49d067a8fca25f09e
Clarekpd <Clarekpd@outlook.com>
2019-11-21 19:11:16 -0600

    correct maths

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumWheelTestControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumWheelTestControls.java
@@ -50,14 +50,14 @@ public class MecanumWheelTestControls extends LinearOpMode {
                 double rightX_G2;
                 double rightY_G2;
 
-                rightX_G1 = gamepad1.right_stick_y;
-                rightY_G1 = -gamepad1.right_stick_x;
-                leftX_G1 = -gamepad1.left_stick_y;
-                leftY_G1 = -gamepad1.left_stick_x;
-                leftX_G2 = gamepad2.left_stick_y;
-                leftY_G2 = gamepad2.left_stick_x;
-                rightX_G2 = gamepad2.right_stick_y;
-                rightY_G2 = gamepad2.right_stick_x;
+            rightY_G1 = -gamepad1.right_stick_y;
+            rightX_G1 = -gamepad1.right_stick_x;
+            leftY_G1 = gamepad1.left_stick_y;
+            leftX_G1 = -gamepad1.left_stick_x;
+            leftY_G2 = gamepad2.left_stick_y;
+            leftX_G2 = gamepad2.left_stick_x;
+            rightY_G2 = gamepad2.right_stick_y;
+            rightX_G2 = gamepad2.right_stick_x;
 
                 if (gamepad1.y) {
                     factor = 1;
@@ -67,45 +67,51 @@ public class MecanumWheelTestControls extends LinearOpMode {
                     factor = 2;
                 }
 
-                if(gamepad1.left_stick_y > 0) {
-                    wheelsOn = true;
-                } else if(gamepad1.left_stick_x > 0) {
-                    wheelsOn = true;
-                } else {
-                    wheelsOn = false;
-                }
+//                if(gamepad1.left_stick_y > 0) {
+//                    wheelsOn = true;
+//                } else if(gamepad1.left_stick_x > 0) {
+//                    wheelsOn = true;
+//                } else {
+//                    wheelsOn = false;
+//                }
 
-//                 if NOPOWER and power applied then state = ACCELERATE, percent = .1
+            //                 if NOPOWER and power applied then state = ACCELERATE, percent = .1
+//                if(power == false) {
+//                    multiplier = 1;
+//                } else if(power == true && nPower > 1) {
+//                    wheelsOn = true;
+//                } else if (nPower >= 1) {
+//                    steady = true;
+//                }
 //                 if ACCELERATE and power applied and percent < 1 then percent = percent + .1
 //                 if ACCELERATE and power applied and percent >= 1 then state = STEADY
 //                 if power not applied then state = NOPOWER
 //                 if state = STEADY then state = STEADY
 //                 set power (power * percent)
-/*
-                if (wheelsOn = true) {
-                    for (int i = 0; i <= 10; i++) {
-                        if (i > 1) {
-                            multiplier = 0.1;
-                        } else if (i > 2 && i < 4) {
-                            multiplier = 0.2;
-                        } else if (i > 4 && i < 6) {
-                            multiplier = 0.4;
-                        } else if (i > 6 && i < 8) {
-                            multiplier = 0.6;
-                        } else if (i > 8 && i < 10) {
-                            multiplier = 0.8;
-                        } else if (i > 10) {
-                            multiplier = 1;
-                            break;
-                        }
-                    }
+
+                boolean power = false;
+                boolean steady = false;
+                double nPower = 0;
+
+                nPower = (tgtPowerLB + tgtPowerLF + tgtPowerRB + tgtPowerRF) / 4;
+
+                if (nPower > 0) {
+                    power = true;
                 }
 
-*/
-                frontLeftMotor.setPower(((leftY_G1 + rightX_G1 - leftX_G1) / factor) * multiplier);
-                backLeftMotor.setPower(((leftY_G1 + rightX_G1 + leftX_G1) / factor) * multiplier);
-                backRightMotor.setPower(((leftY_G1 - rightX_G1 + leftX_G1) / factor) * multiplier);
-                frontRightMotor.setPower(((leftY_G1 - rightX_G1 - leftX_G1) / factor) * multiplier);
+                if (power == true && steady == false && multiplier < 1) {
+                    multiplier = multiplier + 0.1;
+                } else if (power = false) {
+                    multiplier = 0;
+                    steady = false;
+                } else if (multiplier >= 1) {
+                    steady = true;
+                }
+
+            frontLeftMotor.setPower(((rightX_G1 + rightY_G1 - leftX_G1) / factor) * multiplier);
+            backLeftMotor.setPower(((rightX_G1 + rightY_G1 + leftX_G1) / factor) * multiplier);
+            backRightMotor.setPower(((rightX_G1 - rightY_G1 + leftX_G1) / factor) * multiplier);
+            frontRightMotor.setPower(((rightX_G1 - rightY_G1 - leftX_G1) / factor) * multiplier);
 
 
         }
