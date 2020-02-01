87060fa6d3b864117958704e63ae8eaa5af8bcfa
afisher2 <afisher1@gmail.com>
2020-01-31 19:08:31 -0600

    rack in

---
+++ TeamCode/src/main/java/RackBack.java
@@ -0,0 +1,18 @@
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.RepresentoBotSupremeLeader;
+
+@Autonomous
+public class RackBack extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+
+        RepresentoBotSupremeLeader bot = new RepresentoBotSupremeLeader(this);
+        waitForStart();
+
+        bot.timeRackIn(4.5f);
+    }
+}
